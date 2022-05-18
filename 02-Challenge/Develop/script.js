2cbe295164a994761c8e48d1f9e6ffdf33709a42
seanmhuggins <seanhuggins@Seans-MacBook-Pro.local>
2022-05-18 14:20:20 -0600

    created all of the variables needed for the password questions.

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,14 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate").addEventListener("click", createPassword);
+
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbol = ["?", ">", "<", ">", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "-", "+"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+let confirmLength;
+let confirmUppercase;
+let confirmLowercase;
+let confirmNumber;
+let confirmSymbol;
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const str = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"]
+// const avail = []
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sean-password-generator
+DESCRIPTION:
+    In this challenge I created a password generator that produces a random password. To do so, I asked multiple questions regarding the different attributes the user would like to include in their password. These questions included password length, and wether the user wanted to include uppercase letters, lowercase letters, symbols, and numbers.
