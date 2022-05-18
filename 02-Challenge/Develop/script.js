5e609e2def05e0e6d8a4cb02d7ad23f72a01d026
seanmhuggins <seanhuggins@Seans-MacBook-Pro.local>
2022-05-18 14:49:05 -0600

    created functions and if/then statements

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-const generateBtn = document.querySelector("#generate").addEventListener("click", createPassword);
+document.querySelector("#generate").addEventListener("click", createPassword);
 
 const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbol = ["?", ">", "<", ">", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "-", "+"];
@@ -13,14 +13,60 @@ let confirmLowercase;
 let confirmNumber;
 let confirmSymbol;
 
-// Write password to the #password input
+
+function writePassword() {
+  let confirmLength = prompt("How many characters you wish to have in your password?");
+
+  if (confirmLength < 8 || confirmLength > 128){
+  alert("Please select a value between 8 and 128.")
+  confirmLength = prompt("How many characters do you wish to have in your password?")
+  }
+
+  let confirmUppercase = confirm("Press OK if you would like to use uppercase characters.");
+  let confirmLowercase = confirm("Press OK if you would like to use lowercase characters.");
+  let confirmNumber = confirm("Press OK if you would like to use numeric characters.");    
+  let confirmSymbol = confirm("Press OK if you would like to use special characters.");
+
+  if (confirmUppercase === false && confirmLowercase === false && confirmNumber === false && confirmSymbol === false){
+    alert("You are required to select at least one parameter to generate a password.")
+  }
+
+  let randomcharacters = []
+
+  if (confirmUppercase) {
+    randomcharacters = randomcharacters.concat(uppercase)
+  }
+
+  if (confirmLowercase) {
+    randomcharacters = randomcharacters.concat(lowercase)
+  }
+
+  if (confirmNumber) {
+    randomcharacters = randomcharacters.concat(number)
+  }
+
+  if (confirmSymbol) {
+    randomcharacters = randomcharacters.concat(symbol)
+  }
+  console.log(randomcharacters)
 
 
-// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// const str = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"]
-// const avail = []
+  let approvedPassword = ""
+
+  for (let i = 0; i < confirmLength; i++) {
+    approvedPassword = approvedPassword + randomcharacters[Math.floor(Math.random() * randomcharacters.length)];
+    console.log(approvedPassword)
+    
+  }
+
+  return approvedPassword;
+}
+
+function createPassword() {
+  let password = writePassword();
+  let passwordText = document.querySelector("#password");
 
 
+passwordText.value = password;
 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+}
